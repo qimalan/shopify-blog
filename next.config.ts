c8dececa720489e869101195a586a5d29c696047
miles <2604323152@qq.com>
2026-02-26 14:51:25 +0800

    feat: 添加 Next.js 配置以支持导出和图像优化设置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "https://github.com/qimalan/shopify-blog",
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;

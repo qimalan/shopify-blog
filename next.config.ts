c7739eceb074486621cca90abc9524374f9be414
miles <2604323152@qq.com>
2026-02-26 14:59:52 +0800

    fix: 修正 basePath 配置以确保正确的路径设置

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "https://github.com/qimalan/shopify-blog",
+  basePath: "/shopify-blog",
   images: {
     unoptimized: true
   }

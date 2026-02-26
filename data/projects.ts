1a55a6ecfa5eb75a45efcba19ff834b30537320f
miles <2604323152@qq.com>
2026-02-26 22:44:57 +0800

    新增项目

--- data/projects.ts
+++ data/projects.ts
@@ -8,24 +8,17 @@ export interface Project {
 }
 
 export const projects: Project[] = [
+
   {
     id: "0",
-    title: "Goflash",
-    description: "面向海外的购物返利电商平台。",
-    image: "/shopify-blog/goflash.webp",
-    url: "https://goflashdeals.com/",
-    tags: ["React", "Nextjs"],
+    title: "定制首饰独立站",
+    description: "为定制首饰品牌打造的 Shopify 独立站，提供个性化的设计。",
+    image: "/shopify-blog/project3.webp",
+    url: "https://www.fleurshine.com/",
+    tags: ["Shopify Theme", "Custom Design"],
   },
-  // {
-  //   id: "1",
-  //   title: "变体级别补货通知 App",
-  //   description: "为 Shopify 品牌开发的变体级别补货通知应用，提升客户购物体验。",
-  //   image: "/project3.png",
-  //   url: "https://landing.54d.com/",
-  //   tags: ["Shopify App", "React"],
-  // },
   {
-    id: "2",
+    id: "1",
     title: "54D Theme",
     description: "为销售健身课程的品牌搭建的 Shopify 主题。",
     image: "/shopify-blog/project1.webp",
@@ -33,11 +26,19 @@ export const projects: Project[] = [
     tags: ["Shopify Theme", "Custom Design"],
   },
   {
-    id: "3",
+    id: "2",
     title: "二手奢侈品独立站",
     description: "为二手奢侈品品牌打造的 Shopify 独立站，支持多渠道销售与库存管理。",
     image: "/shopify-blog/project2.webp",
     url: "https://miloura.com/",
     tags: ["Shopify Theme", "Liquid"],
+  },
+  {
+    id: "3",
+    title: "Goflash",
+    description: "面向海外的购物返利电商平台。",
+    image: "/shopify-blog/goflash.webp",
+    url: "https://goflashdeals.com/",
+    tags: ["React", "Nextjs"],
   },
 ];
